255f01904ba514573d3a31d2d1dd0e4155a4cb53
Will Boland <will@fyrstertech.com>
2021-06-20 09:55:51 -0400

    creates login and signup VC

---
+++ lint/lint/View Controllers/LoginViewController.swift
@@ -0,0 +1,12 @@
+//
+//  LoginViewController.swift
+//  lint
+//
+//  Created by Will Boland on 6/20/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+}
---
+++ lint/lint/View Controllers/signup.swift
@@ -0,0 +1,12 @@
+//
+//  signup.swift
+//  lint
+//
+//  Created by Will Boland on 6/20/21.
+//
+
+import UIKit
+
+class signup: UIViewController {
+
+}
